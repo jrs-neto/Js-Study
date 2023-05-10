efe6f04930a565701902901ea4f87655615f2111
jrs-neto <jrd.santosneto@gmail.com>
2023-05-10 19:17:54 -0300

    Tipos de comentarios

---
+++ aula4/script.js
@@ -0,0 +1,9 @@
+// Comentários
+
+// Comentário de linha, só funciona para uma linha
+
+/* 
+comentário de bloco, serve para comentar várias linhas
+*/
+
+// ctrl + ; -> Atalho de comentário no Vscode
