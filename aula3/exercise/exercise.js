ccb992d8282853bb6f50d27a0ded1dbfec17d8e5
jrs-neto <jrd.santosneto@gmail.com>
2023-05-10 19:15:29 -0300

    Funcao console.log

---
+++ aula3/exercise/exercise.js
@@ -0,0 +1,3 @@
+// Meu nome é "José". Estou aprendendo JavaScript ás 12 da tarde.
+
+console.log('Meu nome é "josé". Estou aprendendo JavaScript às', 12, 'da tarde.');
---
+++ aula3/script.js
@@ -0,0 +1,13 @@
+// Função console.log
+console.log('José Rodrigues'); // Usar aspas '' ou "" para exibir um valor literal
+console.log("José Rodrigues");
+console.log(`José Rodrigues`);
+console.log("'José Rodrigues'");
+console.log('"José Rodrigues"');
+console.log(`'"José Rodrigues"'`);
+
+// Tudo que for considerado texto em Js é chamado de String
+
+//NUmber
+console.log(123456, 15.85); //Exibição de valor literal
+console.log(15.85); // Utiliza-se ponto para separar n° por casas decimais
