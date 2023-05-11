8665227b9c694eebaa6b738d835a3ea4d563064c
jrs-neto <jrd.santosneto@gmail.com>
2023-05-11 12:07:23 -0300

    Constantes com const

---
+++ aula7/script/script.js
@@ -0,0 +1,22 @@
+// Constantes com const
+
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// O nome de uma constante não pode começar com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// São Case-sensitive (existe diferença entre maiúscula e minúscula)
+// Não podemos modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE CONST.
+
+// Operadores lógicos - + - * /
+
+// String = Text (fica dentro de '') / Number = Número (Fica foda de aspas)
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado +5;
+
+console.log(typeof primeiroNumero);
