6380ffd829332898b7f45f9231e656981bc14e96
jrs-neto <jrd.santosneto@gmail.com>
2023-05-11 12:16:19 -0300

    alert, confirm and prompt

---
+++ aula11/script/script.js
@@ -0,0 +1,12 @@
+// alert, confirm e prompt (Navegador)
+
+//Exercício: Realizar conta de adição
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número:');
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+
+
+//alert(`O resultado da sua operação foi: ${resultado}`);
+alert(`O resultado da sua operação foi: ${num1 + num2}`);
