390e02d65d32e99b13b751976146bb4e2291b28a
jrs-neto <jrd.santosneto@gmail.com>
2023-05-11 12:11:47 -0300

    variaveis com let

--- aula6/script/script.js
+++ aula6/script/script.js
@@ -8,7 +8,7 @@ console.log(nome, 'casou-se com Maria em 2012.');
 console.log('Maria teve 1 filho com', nome, 'em 2015.');
 console.log('O filho de', nome,'se chama Eduardo.');
 
-/*let nome; // Declarou a variável
+/*let nome;  //Declarou a variável
 nome = 'Qualquer valor'; // Inicializou a variável
 console.log(nome);*/
 
