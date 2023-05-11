099a5b98442e00cc5e13a68502fdc3c4fe6d1987
jrs-neto <jrd.santosneto@gmail.com>
2023-05-11 12:06:17 -0300

    Let vs Var

---
+++ aula6/script/script.js
@@ -0,0 +1,22 @@
+// Variáveis com let
+
+let nome = 'Miguel'; // Sting
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome,'se chama Eduardo.');
+
+/*let nome; // Declarou a variável
+nome = 'Qualquer valor'; // Inicializou a variável
+console.log(nome);*/
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// O nome de uma variável não pode começar com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// São Case-sensitive (existe diferença entre maiúscula e minúscula)
+// Não podemos redeclarar uma variável com let
+// NÃO UTILIZE VAR, UTILIZE LET.
